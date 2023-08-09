fde2a31c6e46d74cd05acdacdfd50a54cd5e3cd1
Kyla <kylarosethron@gmail.com>
2023-08-09 16:59:30 -0400

    aesthetic fixes

--- pages/Event_Schedule.py
+++ pages/Event_Schedule.py
@@ -1,7 +1,12 @@
 import streamlit as st
 from PIL import Image
 # Define the HTML for the centered header
-centered_html = '<h1 style="text-align: center;">Tentative Schedule</h1>'
+centered_html = '<h1 style="text-align: center;">Event Schedule</h1>'
+
+col1, col2, col3 = st.columns(3)
+with col2:
+	st.write('October 20-22')
+	
 # Display the centered header using st.markdown
 st.markdown(centered_html, unsafe_allow_html=True)
 		
--- pages/Registration.py
+++ pages/Registration.py
@@ -8,4 +8,6 @@ st.write('---')
 url = 'https://docs.google.com/forms/d/e/1FAIpQLSceenpAtEpVHO0rGEUscBRVER26WRZ9MDMXQKnnGf8DR2btXQ/viewform?usp=sf_link'
 button_label = 'Register Here'
 
-st.markdown(f"[{button_label}]({url})", unsafe_allow_html=True, )
+col1, col2, col3 = st.columns(3)
+with col2:
+	st.markdown(f"[{button_label}]({url})", unsafe_allow_html=True, )
