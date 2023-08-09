f2149a9eb613e0561b66b93e6236b3aaf372573a
Kyla <kylarosethron@gmail.com>
2023-08-09 17:02:21 -0400

    dates

--- pages/Event_Schedule.py
+++ pages/Event_Schedule.py
@@ -3,9 +3,7 @@ from PIL import Image
 # Define the HTML for the centered header
 centered_html = '<h1 style="text-align: center;">Event Schedule</h1>'
 
-col1, col2, col3 = st.columns(3)
-with col2:
-	st.write('October 20-22')
+centered_html2 = '<h2 style="text-align: center;">October 20-22</h2>'
 	
 # Display the centered header using st.markdown
 st.markdown(centered_html, unsafe_allow_html=True)
