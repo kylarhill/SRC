57cf11bfd8ce9010159801974db640d4ec07c961
Kyla <kylarosethron@gmail.com>
2023-09-20 12:31:51 -0400

    format bug fix

--- pages/Registration.py
+++ pages/Registration.py
@@ -1,12 +1,23 @@
 import streamlit as st
 
-st.header('Register Below')
-	
+centered_html = '<h1 style="text-align: center;">Register Below</h1>'
+# Display the centered header using st.markdown
+st.markdown(centered_html, unsafe_allow_html=True)	
 st.write('---')
+
+
 url = 'https://docs.google.com/forms/d/e/1FAIpQLSceenpAtEpVHO0rGEUscBRVER26WRZ9MDMXQKnnGf8DR2btXQ/viewform?usp=sf_link'
 button_label = 'Register Here'
 
+centered_text = '''
+<div style="text-align: center;">
+    :red[Registration is Limited to 20 spots!]
+</div>
+'''
+
+st.markdown(centered_text, unsafe_allow_html=True)
+
+
 col1, col2, col3 = st.columns(3)
 with col2:
-	st.markdown(':red[Registration is Limited to 20 spots!]')
 	st.markdown(f"[{button_label}]({url})", unsafe_allow_html=True, )
