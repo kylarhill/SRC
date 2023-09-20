1c4f71643f54d45feaa4ada7cd4a1e426d50582b
Kyla <kylarosethron@gmail.com>
2023-09-20 12:22:49 -0400

    format editing

--- pages/Registration.py
+++ pages/Registration.py
@@ -1,14 +1,15 @@
 import streamlit as st
 
-col1, col2, col3 = st.columns(3)
+col1 = st.columns(1)
 # Define the HTML for the centered header
-with col2:
+with col1:
 	st.header('Register Below')
-	st.markdown(':red[Registration is Limited to 20 spots!]')
 	
 st.write('---')
 url = 'https://docs.google.com/forms/d/e/1FAIpQLSceenpAtEpVHO0rGEUscBRVER26WRZ9MDMXQKnnGf8DR2btXQ/viewform?usp=sf_link'
 button_label = 'Register Here'
 
+col1, col2, col3 = st.columns(3)
 with col2:
+	st.markdown(':red[Registration is Limited to 20 spots!]')
 	st.markdown(f"[{button_label}]({url})", unsafe_allow_html=True, )
